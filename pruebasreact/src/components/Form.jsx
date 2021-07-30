872cb9b0cbac481f8855d4e7bb2843b3531b8c91
Andyrko1 <hs170505@alumno.udb.edu.sv>
2021-07-30 09:01:05 -0600

    first commit

---
+++ pruebasreact/src/components/Form.jsx
@@ -0,0 +1,29 @@
+import React,{useState} from 'react';
+import Todo from '../components/Todo';
+const Form = () => {
+    const [todo, setTodo]= useState({})
+    const [todos, setTodos] = useState([
+        {todo: 'todo 1'},
+        {todo: 'todo 2'},
+        {todo: 'todo 3'}
+    ])
+    const handleChange = e => setTodo({[e.target.name]: e.target.value})
+    const handleClick = e => console.log('click click')
+return (
+    <>
+{
+    <form onSubmit={e=> e.preventDefault()}>
+        <label>Agregar tarea</label><br />
+        <input type="text" name="todo" onChange={handleChange}/>
+        <button onClick={handleClick}>agregar</button>
+        </form>
+        {
+    todos.map((value,index) => (
+        <Todo todo={value.todo}/>
+        ))
+    }
+
+</>
+)
+}
+export default Form
